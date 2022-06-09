b31b08800965272d3fb3f2e1109d239791efc017
SEANYB4 <bainsean@outlook.com>
2022-06-09 12:44:45 +0100

    Implemented v1.

---
+++ src/InfoCards/Li.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
+function L() {
+
+        return (
+
+
+            <div className="infoCard">
+
+                <h1>Lithium</h1>
+
+                <p>Lithium. lithium.</p>
+            </div>
+        )
+
+
+
+}
+
+
+
+export default L;
---
+++ src/InfoCards/Pb.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
+function H() {
+
+        return (
+
+
+            <div className="infoCard">
+
+                <h1>Hydrogen</h1>
+
+                <p>Hydrogen is an element with atomic mass 1.</p>
+            </div>
+        )
+
+
+
+}
+
+
+
+export default H;
